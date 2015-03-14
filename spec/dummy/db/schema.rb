418959d1704d4dc43d0b17ce800a616460ededb0
Marcelo Piva <m.pivaa@gmail.com>
2015-03-14 02:26:50 -0300

    Change bitters base location and nav links

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,13 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312181626) do
+ActiveRecord::Schema.define(version: 20150313165836) do
 
-  create_table "tingui_posts", force: :cascade do |t|
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+
+  create_table "tingui_entries", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "slug"
+    t.string   "type",       default: "post", null: false
   end
+
+  add_index "tingui_entries", ["slug"], name: "index_tingui_entries_on_slug", unique: true
+  add_index "tingui_entries", ["type"], name: "index_tingui_entries_on_type"
 
 end
